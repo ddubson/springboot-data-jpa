612eb279408230fbb5c01c2e50ccc046be56cf96
Dmitriy Dubson <d.dubson@gmail.com>
2017-09-01 21:49:58 -0400

    Fixed entity resolution issue

--- src/main/kotlin/com/ddubson/models/ServiceRequest.kt
+++ src/main/kotlin/com/ddubson/models/ServiceRequest.kt
@@ -9,7 +9,7 @@ import javax.persistence.*
 @Entity
 @Table(name = "service_requests")
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ServiceRequest(@JsonProperty("unique_key") @Id @Column(name="uniqueKey") @GeneratedValue val uniqueKey: Long?,
+data class ServiceRequest(@JsonProperty("unique_key") @Id @GeneratedValue @Column(name="uniqueKey") val uniqueKey: Long,
                           @JsonProperty("created_date") @Column(name="createdDate") val createdDate: Timestamp?,
                           @JsonProperty("closed_date") @Column(name="closedDate") val closedDate: Timestamp?,
                           @JsonProperty("address_type") @Column(name="addressType") val addressType: String?,
--- src/main/resources/db/migration/V1__initialSchema.sql
+++ src/main/resources/db/migration/V1__initialSchema.sql
@@ -1,9 +1,8 @@
 CREATE TABLE service_requests (
-  uniqueKey   BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uniqueKey   BIGINT        NOT NULL PRIMARY KEY AUTO_INCREMENT,
   createdDate TIMESTAMP,
   closedDate  TIMESTAMP,
   addressType VARCHAR(50) NOT NULL,
   agencyName  VARCHAR(100),
   agency      VARCHAR(50) NOT NULL
 );
-
